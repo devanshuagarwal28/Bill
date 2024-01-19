d93551b6b6c70df0a946999ec156dd3513e4c8f1
Vaibhav Kumar <catvaku@gmail.com>
2024-01-19 22:33:44 +0530

    no change

--- client/web/server/file/node/server.js
+++ client/web/server/file/node/server.js
@@ -4,7 +4,7 @@ import { readConfig } from 'utils/my_config'
 let myConfig = readConfig(process.argv[2])
 
 let server = http.createServer((req, res) => {
-  res.end(`HELLO FROM SERVER ${JSON.stringify(myConfig)}`)
+  res.end(`HELLO FROM SERVER ${JSON.stringify(myConfig)} ${JSON.stringify(process.env)}`)
 })
 
 server.listen(myConfig["host"]["port"], myConfig["host"]["address"], _ => {
